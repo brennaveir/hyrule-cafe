7114f1177dc084c716544c7f257c877b3aa223b7
brennaveir <brennaveir@hotmail.com>
2023-07-04 12:02:20 -0400

    added logic to map over menu items and display dynamically

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,47 @@
 const menu = [
     {
-       item: "Mushroom Skewer",
+       name: "Mushroom Skewer",
        category: "Entree",
-       description: "This simple mushroom-packed skewer has its colorful presentation to thank for its appeal."
+       description: "This simple mushroom-packed skewer has its colorful presentation to thank for its appeal.",
+       image: "./assets/images/mushroomSkewer.png"
     },
     {
-        item: "Milk",
+        name: "Milk",
         category: "Beverage",
-        description: "Drink it before bed to ensure a good night's sleep."
+        description: "Drink it before bed to ensure a good night's sleep.",
+        image: "./assets/images/milk.png"
     },
     {
-        item: "Fruit Pie",
+        name: "Fruit Pie",
         category: "Dessert",
-        description: "A celebration isn't a celebration until this fruit-filled crust hits the table!"
+        description: "A celebration isn't a celebration until this fruit-filled crust hits the table!",
+        image: "./assets/images/fruitPie.png"
     }
 ]
+
+const menuSection = document.querySelector('.menu');
+
+window.addEventListener('DOMContentLoaded', function () {
+    let showMenu = menu.map(function (item) {
+        console.log(item)
+        return ` <article class="singleItem">
+        <!-- single menu item -->
+        <!-- image -->
+<img src=${item.image} alt=${item.name} />
+        <div class="item-info">
+            <!-- item info -->
+            <header>
+                <h4>
+                    ${item.name}
+                </h4>
+                </header>
+                <p class="item-descr">
+                    <!-- item description -->
+                    ${item.description}
+                </p>
+            </div>
+    </article>`;
+    });
+    showMenu = showMenu.join('');
+    menuSection.innerHTML = showMenu;
+})
