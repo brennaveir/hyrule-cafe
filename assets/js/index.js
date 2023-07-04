1b2b02f87b164b2e9835d93d019af07a1618b7da
brennaveir <brennaveir@hotmail.com>
2023-07-04 16:04:12 -0400

    added filtering option for menu items

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,53 @@
 const menu = [
     {
        name: "Mushroom Skewer",
-       category: "Entree",
+       category: "entree",
        description: "This simple mushroom-packed skewer has its colorful presentation to thank for its appeal.",
        image: "./assets/images/mushroomSkewer.png"
     },
     {
         name: "Milk",
-        category: "Beverage",
+        category: "beverage",
         description: "Drink it before bed to ensure a good night's sleep.",
         image: "./assets/images/milk.png"
     },
     {
         name: "Fruit Pie",
-        category: "Dessert",
+        category: "dessert",
         description: "A celebration isn't a celebration until this fruit-filled crust hits the table!",
         image: "./assets/images/fruitPie.png"
     }
 ]
 
 const menuSection = document.querySelector('.menu');
+const filterBtns = document.querySelectorAll('.filter-btn')
 
+//load items
 window.addEventListener('DOMContentLoaded', function () {
     displayMenuItems(menu)
 });
 
+//filter items
+filterBtns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function(menuItem) {
+            if (menuItem.category === category) {
+                return menuItem
+            }
+        });
+        if (category === 'all') {
+            displayMenuItems(menu)
+        } else {
+            displayMenuItems(menuCategory)
+        }
+    });
+});
+
+
 function displayMenuItems (menuItems) {
     let showMenu = menuItems.map(function (item) {
-        console.log(item)
+        // console.log(item)
         return ` <article class="singleItem">
         <!-- single menu item -->
         <!-- image -->
