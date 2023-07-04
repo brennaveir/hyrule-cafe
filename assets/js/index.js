c4ee3c9e7d1cd09bd39819d621d4a4ebee6d7087
brennaveir <brennaveir@hotmail.com>
2023-07-04 11:24:40 -0400

    added test menu items

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+const menu = [
+    {
+       item: "Mushroom Skewer",
+       category: "Entree",
+       description: "This simple mushroom-packed skewer has its colorful presentation to thank for its appeal."
+    },
+    {
+        item: "Milk",
+        category: "Beverage",
+        description: "Drink it before bed to ensure a good night's sleep."
+    },
+    {
+        item: "Fruit Pie",
+        category: "Dessert",
+        description: "A celebration isn't a celebration until this fruit-filled crust hits the table!"
+    }
+]
